0f7aab307f2bb334b868c72e128a251de012aaab
Fable Tales <penelope@hey.com>
2022-12-18 15:08:04 -0500

    Bump rubyfmt to 0.8.1

--- Formula/rubyfmt.rb
+++ Formula/rubyfmt.rb
@@ -2,8 +2,8 @@ class Rubyfmt < Formula
   desc "Ruby autoformatter"
   homepage "https://github.com/penelopezone/rubyfmt"
   url "https://github.com/penelopezone/rubyfmt.git",
-    tag:      "v0.8.0",
-    revision: "ed99cc4586a908c97f8b19ed78801342f7aa8512"
+    tag:      "v0.8.1",
+    revision: "b1440a576995b057bdad47cb546abc021376b59d"
   license "MIT"
   head "https://github.com/penelopezone/rubyfmt.git", branch: "trunk"
 
@@ -22,16 +22,7 @@ class Rubyfmt < Formula
   depends_on "bison" => :build
   depends_on "rust" => :build
   uses_from_macos "ruby"
-
-  # This patch includes a fix for Big Sur ARM builds which was not included in the 0.8.0
-  # release. This should be removed for future releases, which should include this patch.
-  patch do
-    on_big_sur do
-      url "https://github.com/penelopezone/rubyfmt/commit/8a193552e6b8232d44347505f4cd503c800161a3.patch?full_index=1"
-      sha256 "936734916d24233c03e986ded729b2cc0a3ebce4180136bcfd845dfef8b1f4c5"
-    end
-  end
-
+  
   def install
     system "cargo", "install", *std_cargo_args
     bin.install "target/release/rubyfmt-main" => "rubyfmt"
